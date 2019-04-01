29ab9daa96183193bd1e652d49e1df52a4e8bd46
Pengfei Wang <13671619364@sina.cn>
2019-04-01 17:40:19 +0800

    添加部分数据结构

--- canvas.js
+++ canvas.js
@@ -1,15 +1,54 @@
-var hello1 = (function () {
-    var hello1 = function () {
-        alert(1);
-    };
-     var hello1str = 'i am hello1';
-    return {
-        hello1: hello1,
-        hello1str: hello1str        
-    };
-})();
-
+const rowCnt = 24;
+const colCnt = 32;
 var canvas = (function(){
 	var mainUi = document.getElementById("mainUI");
 	return mainUi;
 })();
+
+var height = canvas.offsetHeight;
+var width = canvas.offsetWidth;
+var cellWidth = width / colCnt;
+var cellHeight = width / rowCnt;
+var snake = {
+	body = [];
+}
+
+var food;
+
+//Class Vector
+var Vector = function(x, y){
+	var X = x;
+	Var Y = y;
+}
+
+Vector.prototype = {
+	constructor = Vector;
+}
+
+//Class Point
+var Point = function(x, y){
+	var X = x;
+	var Y = y;
+}
+
+Point.prototype = {
+	constructor = Point;
+	toString() = function(){
+		return "(" + X.toString() + "," +Y.toString + ")";
+	}
+	var moveVect(vec){
+		X += vec.X;
+		Y += vec.Y;
+	}
+}
+
+var getRandFoodPos = function(){
+	var foodX = Math.floor(Math.Random() * rowCnt);
+	var foodY = Math.floor(Math.Random() * colCnt);
+	return new Point(foodX, foodY);
+}
+
+var init = function(){
+	food = getRandFoodPos();
+
+}
